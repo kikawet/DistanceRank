814a8af238f290be02e1cf87325f2dac10ea39c6
flo00008 <lopezortegafrancisco1@gmail.com>
2020-03-03 20:38:28 +0100

    Crawler acabado

---
+++ src/crawler.py
@@ -0,0 +1,56 @@
+import scrapy
+from scrapy import signals
+import json
+import re
+
+
+class URL_Crawler(scrapy.Spider):
+    name = 'url_crawler'
+    start_urls = ['http://brickset.com/sets/year-2016']
+    __urls = {}
+
+
+
+    @classmethod
+    def from_crawler(cls, crawler, *args, **kwargs):
+        spider = super(URL_Crawler, cls).from_crawler(crawler, *args, **kwargs)
+        crawler.signals.connect(spider.spider_closed, signal=signals.spider_closed)
+        spider.max_size = int(spider.max_size)
+        return spider
+
+    def spider_closed(self, spider):
+        spider.logger.info('Crawling acabado guardando en archivo')
+        urls = set(self.getDomain(link) for link in self.__urls.keys())
+
+        filtered_urls = {}
+        for k, v in self.__urls.items():
+            intenseccion = urls.intersection(v)
+            if len(intenseccion) != 0:
+                filtered_urls[self.getDomain(k)] = list(intenseccion)
+
+        with open(self.output + '/urls.json', 'w') as f:
+            json.dump(filtered_urls, f)
+
+    def parse(self, response):
+        PAGE_SELECTOR = "//a/@href"
+        URL_SELECTOR = r'https?://(?:[-\w.]|(?:%[\da-fA-F]{2}))+'
+
+        if len(self.__urls) < self.max_size and response.url not in self.__urls:
+            links = response.xpath(PAGE_SELECTOR).re(URL_SELECTOR)
+
+            if len(links) != 0:
+                filtered_links = set(self.getDomain(link) for link in links)
+                self.__urls[response.url] = filtered_links
+                # links = list(links)
+
+                for link in links:
+                    filtered = self.getDomain(link)
+                    if filtered not in self.__urls:
+                        yield scrapy.Request(
+                            url=link,
+                            callback=self.parse
+                        )
+
+    def getDomain(self, url):
+        DOMAIN_SELECTOR = r'[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}'
+        return re.search(DOMAIN_SELECTOR, url).group(0)
